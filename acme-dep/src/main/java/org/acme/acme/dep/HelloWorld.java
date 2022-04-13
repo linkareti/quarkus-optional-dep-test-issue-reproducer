730f8b755f12710ad746fd6f46fef0d9e976de7a
Bruno Gonçalves <bnazare@linkare.com>
2022-04-13 18:50:50 +0100

    added issue with optional dependency and jandex upgraded quarkus version

--- acme-dep/src/main/java/org/acme/acme/dep/HelloWorld.java
+++ acme-dep/src/main/java/org/acme/acme/dep/HelloWorld.java
@@ -1,5 +1,18 @@
 package org.acme.acme.dep;
 
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfStamper;
+
 public class HelloWorld {
-    
+
+    public void hello() {
+        try {
+            PdfStamper stamper = PdfStamper.createSignature(null, null, '\0');
+        } catch (IOException | DocumentException e) {
+            e.printStackTrace();;
+        }
+    }
+
 }
